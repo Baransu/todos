061dd35c9a4f3e845543524a44c76c705d52266d
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-02-18 14:38:47 +0100

    phoenix and elm scaffold

---
+++ web/elm/Main.elm
@@ -0,0 +1,38 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, text, button)
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = No0p
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text "hello world"
+        ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = (\_ -> Sub.none)
+        }
---
+++ web/static/js/app.js
@@ -0,0 +1,2 @@
+const root = document.getElementById('elm-root');
+const elmMain = Elm.Main.embed(root);
